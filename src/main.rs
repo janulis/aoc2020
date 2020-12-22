6f5db534978035e05fdf2254e019409dbe52b4a8
janulis <mindaugas.janulis@gmail.com>
2020-12-22 22:29:20 +0100

    Add day 8 solution

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,13 @@ mod day4;
 mod day5;
 mod day6;
 mod day7;
+mod day8;
 mod utils;
 
 fn help() {
     println!(
         "Usage: ./aoc2020 <day_num> <part_num>
-        where <day_num>: [1..6], <part_num>: [1..2]"
+        where <day_num>: [1..8], <part_num>: [1..2]"
     );
 }
 
@@ -49,6 +50,8 @@ fn main() {
         (6, 2) => day6::part2(),
         (7, 1) => day7::part1(),
         (7, 2) => day7::part2(),
+        (8, 1) => day8::part1(),
+        (8, 2) => day8::part2(),
         _ => help(),
     }
 }
